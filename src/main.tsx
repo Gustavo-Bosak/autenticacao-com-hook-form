733b50010d895b019e105ce6e307f6e30b2aab1e
Gustavo-Bosak <gustavobosaksantos@gmail.com>
2025-10-03 17:57:19 -0300

    :sparkles: feat: construção das rotas em main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,34 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Error from './routes/Error/index.tsx'
+import Login from './routes/Login/index.tsx'
+import Cadastro from './routes/Cadastro/index.tsx'
+import Home from './routes/Home/index.tsx'
+
+const router = createBrowserRouter([{
+  element: <App/>,
+  errorElement: <Error/>,
+  path: '/',
+  children: [
+    {
+      element: <Login/>,
+      path: ''
+    },
+    {
+      element: <Cadastro/>,
+      path: '/cadastro'
+    },
+    {
+      element: <Home/>,
+      path: '/home'
+    }
+  ]
+}])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router}/>
+  </StrictMode>
 )
