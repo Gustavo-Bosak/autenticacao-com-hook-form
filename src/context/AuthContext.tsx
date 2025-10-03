65252477a5710a49635cc62abea6906f8b4b42e9
Gustavo-Bosak <gustavobosaksantos@gmail.com>
2025-10-03 19:28:08 -0300

    :sparkles: feat(login): adiciona AuthContext

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,32 @@
+import React, { createContext, useContext, useState } from 'react'
+import type { tipoContext } from '../types/tipoContext'
+import type { tipoFuncionario } from '../types/tipoFuncionario'
+
+const AuthContext = createContext<tipoContext | undefined>(undefined)
+
+export function AuthProvider ({ children }: { children: React.ReactNode }) {
+    const [funcionario, setFuncionario] = useState<tipoFuncionario | null>(null)
+    const login = () => {
+        setFuncionario(funcionario)
+    }
+
+    const logout = () => {
+        setFuncionario(null)
+    }
+
+    return (
+        <AuthContext.Provider value={{ funcionario, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuth = (): tipoContext => {
+    const context = useContext(AuthContext);
+
+    if (!context) {
+        throw new Error('useAuth precisa estar dentro de um AuthProvider');
+    }
+
+    return context;
+}
