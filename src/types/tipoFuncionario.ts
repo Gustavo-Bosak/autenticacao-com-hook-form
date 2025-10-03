9175a3536797136aad5abc4f0463bcd35e1bd464
Gustavo-Bosak <gustavobosaksantos@gmail.com>
2025-10-03 18:20:21 -0300

    :sparkles: feat: cria tipo para tabela funcionarios

---
+++ src/types/tipoFuncionario.ts
@@ -0,0 +1,10 @@
+export type tipoFuncionario = {
+    nome:string;
+    rf:string;
+    cargo:string;
+    setor:string;
+    cpf:string;
+    telefone:string;
+    email:string;
+    salario:number;
+}
