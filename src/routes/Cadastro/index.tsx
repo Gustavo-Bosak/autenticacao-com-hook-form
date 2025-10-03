57ae3482a276145bd9cc6114ab6170ee9c495e12
Gustavo-Bosak <gustavobosaksantos@gmail.com>
2025-10-03 20:58:00 -0300

    :reclycle: refactor: atualiza estrutura de form
    Pre construção do sistema de coleta com formulários

--- src/routes/Cadastro/index.tsx
+++ src/routes/Cadastro/index.tsx
@@ -1,132 +1,84 @@
-function Cadastro() {
+function Cadastro () {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+  }
+
   return (
     <main>
-      <section className='form'>
-          <fieldset>
-            <div>
-              <div className='input-container'>
-                <label htmlFor='idNomeCompleto'>
-                  NomeCompleto
-                </label>
-                <input
-                  type='text'
-                  name="nomeCompleto"
-                  id='idNomeCompleto'
-                  required
-                />
-              </div>
-              <div className='input-container'>
-                <label htmlFor='idRf'>
-                  Rf (Registro_Funcionario)
-                </label>
-                <input
-                  type='text'
-                  name='Rf'
-                  id='idRf'
-                  required
-                />
-              </div>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <div>
+            <div className='input-container'>
+              <label htmlFor='idNomeCompleto'>NomeCompleto</label>
+              <input
+                type='text'
+                name='nomeCompleto'
+                id='idNomeCompleto'
+                required
+              />
             </div>
-            <div>
-              <div className='input-container'>
-                <label htmlFor='idCargo'>
-                  Cargo
-                </label>
-                <input
-                  type='text'
-                  name='Cargo'
-                  id='idCargo'
-                  required
-                />
-              </div>
-              <div className='input-container'>
-                <label htmlFor='idSetor'>Setor</label>
-                <input
-                  type='text'
-                  name='Setor'
-                  id='idSetor'
-                />
-              </div>
+            <div className='input-container'>
+              <label htmlFor='idRf'>Rf (Registro_Funcionario)</label>
+              <input type='text' name='Rf' id='idRf' required />
             </div>
-            <div>
-              <div className='input-container'>
-                <label htmlFor='idCpf'>
-                  CPF 
-                </label>
-                <input
-                  type='text'
-                  name='Cpf'
-                  id='idCpf'
-                  required
-                />
-              </div>
-              <div className='input-container'>
-                <label htmlFor='idTelefone'>
-                  Telefone
-                </label>
-                <input
-                  type='tel'
-                  name='telefone'
-                  id='idTelefone'
-                  required
-                />
-              </div>
+          </div>
+          <div>
+            <div className='input-container'>
+              <label htmlFor='idCargo'>Cargo</label>
+              <input type='text' name='Cargo' id='idCargo' required />
             </div>
-            <div>
-              <div className='input-container'>
-                <label htmlFor='idEmailCorporativo'>
-                  Email Corporativo 
-                </label>
-                <input
-                  type='email'
-                  name='emailCorporativo'
-                  id='idEmailCorporativo'
-                  required
-                />
-              </div>
-              <div className='input-container'>
-                <label htmlFor='idSalario'>
-                  Salário
-                </label>
-                <input
-                  type='number'
-                  name='salário'
-                  id='idSalario'
-                  required
-                />
-              </div>
+            <div className='input-container'>
+              <label htmlFor='idSetor'>Setor</label>
+              <input type='text' name='Setor' id='idSetor' />
             </div>
-            <div>
-              <div className='input-container w-full'>
-                <label htmlFor='idSenha'>
-                  Senha
-                </label>
-                <input
-                  type='password'
-                  name='senha'
-                  id='idSenha'
-                  required
-                />
-              </div>
+          </div>
+          <div>
+            <div className='input-container'>
+              <label htmlFor='idCpf'>CPF</label>
+              <input type='text' name='Cpf' id='idCpf' required />
             </div>
-              <div className='input-container'>
-                <label htmlFor='idSenhaConfirmada'>
-                  Confirmar Senha{' '}
-                  <span className='text-red-500 font-bold'>*</span>
-                </label>
-                <input
-                  type='password'
-                  name='senhaConfirmado'
-                  id='idSenhaConfirmada'
-                  required
-                />
-              </div>
-          </fieldset>
-          <button type='submit'>Cadastrar Colaborador</button>
-      </section>
+            <div className='input-container'>
+              <label htmlFor='idTelefone'>Telefone</label>
+              <input type='tel' name='telefone' id='idTelefone' required />
+            </div>
+          </div>
+          <div>
+            <div className='input-container'>
+              <label htmlFor='idEmailCorporativo'>Email Corporativo</label>
+              <input
+                type='email'
+                name='emailCorporativo'
+                id='idEmailCorporativo'
+                required
+              />
+            </div>
+            <div className='input-container'>
+              <label htmlFor='idSalario'>Salário</label>
+              <input type='number' name='salário' id='idSalario' required />
+            </div>
+          </div>
+          <div>
+            <div className='input-container w-full'>
+              <label htmlFor='idSenha'>Senha</label>
+              <input type='password' name='senha' id='idSenha' required />
+            </div>
+          </div>
+          <div className='input-container'>
+            <label htmlFor='idSenhaConfirmada'>
+              Confirmar Senha <span className='text-red-500 font-bold'>*</span>
+            </label>
+            <input
+              type='password'
+              name='senhaConfirmado'
+              id='idSenhaConfirmada'
+              required
+            />
+          </div>
+        </fieldset>
+        <button type='submit'>Cadastrar Colaborador</button>
+      </form>
     </main>
   )
 }
 
 export default Cadastro
-
--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -1,35 +1,35 @@
-function Login() {
+function Login () {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+  }
+
   return (
-<main>
-      <section className='form'>
-          <fieldset>
-            <div>
-              <div className='input-container'>
-                <label htmlFor='idEmailCorporativo'>
-                  Email <span className='text-red-500 font-bold'>*</span>
-                </label>
-                <input
-                  type='email'
-                  name='emailCorporativo'
-                  id='idEmailCorporativo'
-                />
-              </div>
+    <main>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <div>
+            <div className='input-container'>
+              <label htmlFor='idEmailCorporativo'>
+                Email <span className='text-red-500 font-bold'>*</span>
+              </label>
+              <input
+                type='email'
+                name='emailCorporativo'
+                id='idEmailCorporativo'
+              />
             </div>
-            <div>
-              <div className='input-container'>
-                <label htmlFor='idSenha'>
-                  Senha <span className='text-red-500 font-bold'>*</span>
-                </label>
-                <input
-                  type='password'
-                  name='senha'
-                  id='idSenha'
-                />
-              </div>
+          </div>
+          <div>
+            <div className='input-container'>
+              <label htmlFor='idSenha'>
+                Senha <span className='text-red-500 font-bold'>*</span>
+              </label>
+              <input type='password' name='senha' id='idSenha' />
             </div>
-          </fieldset>
-          <button type='submit'>Entrar</button>
-      </section>
+          </div>
+        </fieldset>
+        <button type='submit'>Entrar</button>
+      </form>
       <p className='mb-[2vh]'>
         Não tem um perfil?{' '}
         {/*<Link to='/cadastrar' className='font-bold'>
