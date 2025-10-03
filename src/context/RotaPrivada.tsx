f4d3464afaeb31544ea8fcf5e75073fb2fb2b044
Gustavo-Bosak <gustavobosaksantos@gmail.com>
2025-10-03 20:37:42 -0300

    :sparkles: feat(routes): adiciona verificação de rotas
    Componente de verificação para redirecionar caso tente acessar rota sem estar logado/já estando logado.

---
+++ src/context/RotaPrivada.tsx
@@ -0,0 +1,19 @@
+import type React from "react"
+import { useAuth } from "./AuthContext";
+import { Navigate } from "react-router-dom";
+
+function RotaPrivada({children, privado}:{children:React.JSX.Element; privado:boolean}):React.JSX.Element {
+    const { funcionario } = useAuth();
+
+    if (privado && !funcionario) {
+        return <Navigate to='/' replace/>
+    }
+    
+    if (!privado && funcionario) {
+        return <Navigate to='/home' replace/>
+    }
+
+    return children
+}
+
+export default RotaPrivada
--- src/main.tsx
+++ src/main.tsx
@@ -7,29 +7,44 @@ import Error from './routes/Error/index.tsx'
 import Login from './routes/Login/index.tsx'
 import Cadastro from './routes/Cadastro/index.tsx'
 import Home from './routes/Home/index.tsx'
+import RotaPrivada from './context/RotaPrivada.tsx'
+import { AuthProvider } from './context/AuthContext.tsx'
 
-const router = createBrowserRouter([{
-  element: <App/>,
-  errorElement: <Error/>,
-  path: '/',
-  children: [
-    {
-      element: <Login/>,
-      path: ''
-    },
-    {
-      element: <Cadastro/>,
-      path: '/cadastro'
-    },
-    {
-      element: <Home/>,
-      path: '/home'
-    }
-  ]
-}])
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    errorElement: <Error />,
+    path: '/',
+    children: [
+      {
+        element:
+          <RotaPrivada privado={false}>
+            <Login />
+          </RotaPrivada>,
+        path: ''
+      },
+      {
+        element:
+          <RotaPrivada privado={false}>
+            <Cadastro />
+          </RotaPrivada>,
+        path: '/cadastro'
+      },
+      {
+        element:
+          <RotaPrivada privado>
+            <Home />
+          </RotaPrivada>,
+        path: '/home'
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>
 )
