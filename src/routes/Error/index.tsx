9905560a9cc6514e02dfed01dcbc8fea74ecac9f
NikolasBrisola <nikolas.brisola@gmail.com>
2025-10-05 17:54:44 -0300

    :sparkles: feat: adiciona funcao pag error

--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -1,7 +1,18 @@
-export default function Error(){
+import { useEffect } from "react"
+
+function Error(){
+    useEffect(() => {
+        document.title = 'Erro 404'
+    },[])
+
     return(
         <main>
-            <h1>Not Found - Erro 404</h1>
+            <section>
+              <h1 className="text-fiap-cor text-6xl">404</h1>
+              <p>A página que você tentou acessar não pode ser encontrada.</p>
+          </section>
         </main>
     )
 }
+
+export default Error
