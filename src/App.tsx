421338b1909105f8722e83889a90614c8fb8130a
Gustavo-Bosak <gustavobosaksantos@gmail.com>
2025-10-05 12:03:37 -0300

    :sparkles: feat: adiciona doc.titulo dinâmico

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import { Outlet, useLocation } from "react-router-dom"
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
+import { useEffect } from "react";
 
 function App() {
   const location = useLocation()
   const acessoPrivado = location.pathname != '/login' && location.pathname != '/cadastro';
+
+  useEffect(()=>{
+    switch (location.pathname) {
+      case '/':
+        document.title = `FIAP Funcionários - Home`
+        break
+      case '/login':
+        document.title = `FIAP Funcionários - Login`
+        break
+      case '/cadastro':
+        document.title = `FIAP Funcionários - Criar conta`
+        break
+      default:
+        document.title = `FIAP Funcionários`
+    }
+
+  },[location.pathname])
 
   return (
     <div>
